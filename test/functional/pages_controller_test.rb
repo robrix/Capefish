6acfa26818d250e620ae1e00eb399b3357582a4f
Rob Rix <rix.rob@gmail.com>
2008-09-15 06:05:56 -0400

    Updated functional test for the pages controller.

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -1,16 +1,6 @@
-require File.dirname(__FILE__) + '/../test_helper'
-require 'pages_controller'
+require 'test_helper'
 
-# Re-raise errors caught by the controller.
-class PagesController; def rescue_action(e) raise e end; end
-
-class PagesControllerTest < Test::Unit::TestCase
-  def setup
-    @controller = PagesController.new
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new
-  end
-
+class PagesControllerTest < ActionController::TestCase
   # Replace this with your real tests.
   def test_truth
     assert true
