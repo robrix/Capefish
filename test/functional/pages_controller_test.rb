c4e9cde55d09cb2ebed2508db9c6501ae4d3cfed
Rob Rix <rix.rob@gmail.com>
2008-11-26 04:13:29 -0500

    Adding some tests.

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-	def test_truth
-		assert true
+	def test_authenticates_access_to_index
+		get :index
+		assert_response 401
+	end
+	
+	def test_authenticates_access_to_destroy
+		post :destroy, :id => "foo"
+		assert_response 401
 	end
 end
