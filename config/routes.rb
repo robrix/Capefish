549af14a3e1aeef4555b362df1372aaa2310efb0
Rob Rix <rix.rob@gmail.com>
2008-11-26 04:09:27 -0500

    s/map.main '',/map.root/

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 	map.resources :pages
 	
-	map.main '', :controller => "pages", :action => "show" # shows the first page, i.e. the one with the lowest id
+	map.root :controller => "pages", :action => "show" # shows the first page, i.e. the one with the lowest id
 	
 	map.connect '*path.:format', :controller => "pages", :action => "show"
 	map.connect '*path', :controller => "pages", :action => "show"
