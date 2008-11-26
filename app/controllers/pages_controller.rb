a7649b46d7cd57424ebccff6051e7324b0cb6a3b
Rob Rix <rix.rob@gmail.com>
2008-11-26 05:01:59 -0500

    PagesController no longer tries to unescape anything for show; show always gets a path. For edit and update it actually calls CGI::unescape rather than just unescape, too.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,7 +20,7 @@ class PagesController < ApplicationController
 	end
 	
 	def show
-		path = params[:id] ? unescape(params[:id]) : params[:path] || []
+		path = params[:path]
 		@page = Page.find(:path => path) || error
 		respond_to do |format|
 			format.html do
@@ -34,12 +34,12 @@ class PagesController < ApplicationController
 	end
 	
 	def edit
-		@page = Page.find(:path => unescape(params[:id]).split("/")) || error
+		@page = Page.find(:path => CGI::unescape(params[:id]).split("/")) || error
 		render :layout => @page.layout_template || self.class.default_layout_template
 	end
 	
 	def update
-		@page = Page.find(:path => unescape(params[:id]).split("/")) || error
+		@page = Page.find(:path => CGI::unescape(params[:id]).split("/")) || error
 		File.open(Page.file_path(@page.path), "w") do |file|
 			file.write params[:page_content]
 		end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 	map.resources :pages
 	
-	map.root :controller => "pages", :action => "show" # shows the first page, i.e. the one with the lowest id
+	map.root :controller => "pages", :action => "show"
 	
 	map.connect '*path.:format', :controller => "pages", :action => "show"
 	map.connect '*path', :controller => "pages", :action => "show"
