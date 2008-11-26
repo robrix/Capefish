71e85defdc5ad04bee83ea1b1cc205013643d5c2
Rob Rix <rix.rob@gmail.com>
2008-11-26 03:55:18 -0500

    Removed many obsolete routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-	map.login 'login', :controller => "sessions", :action => "new"
-	map.create_session 'sessions/create', :controller => "sessions", :action => "create"
-	map.logout 'logout', :controller => "sessions", :action => "destroy"
-	
-	map.resources :users
 	map.resources :pages
-	map.resources :tags
-	
-	map.resources :galleries do |galleries|
-		galleries.resources :photos
-	end
-	
-	map.connect 'tags/*path', :controller => "tags", :action => "show"
 	
 	map.main '', :controller => "pages", :action => "show" # shows the first page, i.e. the one with the lowest id
 	
