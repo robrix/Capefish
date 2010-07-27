9ac09dba3211bc7274edead8dbbc140b0794f49f
Rob RIx <rob@monochromeindustries.com>
2010-07-27 15:04:57 -0400

    Updated the root route style.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do |map|
   resources :pages, :controller => "capefish/pages", :only => [:show, :index]
 	
-	map.root :controller => "capefish/pages", :action => "show"
+	root :to => "capefish/pages#show"
 	
   # map.connect '*path.:format', :controller => "capefish/pages", :action => "show"
 	map.connect '*path', :controller => "capefish/pages", :action => "show"
